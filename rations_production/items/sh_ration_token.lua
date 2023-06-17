621f65d5fbeb6953bac175eacd1eab44c2e8846a
WizardGote <InvaGirSP@gmail.com>
2023-06-18 02:55:13 +0300

    ummmmm

---
+++ rations_production/items/sh_ration_token.lua
@@ -0,0 +1,12 @@
+ITEM.name = "Ration Token"
+ITEM.description = "A small coin token that can be used to receive a plastic for the ration production proccess"
+ITEM.model = "models/gibs/shield_scanner_gib1.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.iconCam = {
+	pos = Vector(509.64, 427.61, 310.24),
+	ang = Angle(25, 220, 0),
+	fov = 0.57
+}
+ITEM.category = "Utility"
+ITEM.price = 0
